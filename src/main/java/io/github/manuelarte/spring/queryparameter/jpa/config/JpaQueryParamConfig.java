50e785db5cde88f6b73259d258eac75a9d4ac4eb
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2020-04-22 10:03:47 +0200

    Refactor to use model-0.0.2

--- src/main/java/io/github/manuelarte/spring/queryparameter/jpa/config/JpaQueryParamConfig.java
+++ src/main/java/io/github/manuelarte/spring/queryparameter/jpa/config/JpaQueryParamConfig.java
@@ -8,6 +8,8 @@ import io.github.manuelarte.spring.queryparameter.jpa.operatorpredicate.DefaultI
 import io.github.manuelarte.spring.queryparameter.jpa.operatorpredicate.DefaultLowerThanOrEqualsPredicate;
 import io.github.manuelarte.spring.queryparameter.jpa.operatorpredicate.DefaultLowerThanPredicate;
 import io.github.manuelarte.spring.queryparameter.jpa.operatorpredicate.OperatorPredicate;
+import io.github.manuelarte.spring.queryparameter.jpa.transformers.QueryCriteriaToSpecificationTransformer;
+import io.github.manuelarte.spring.queryparameter.model.TypeTransformerProvider;
 import io.github.manuelarte.spring.queryparameter.operators.EqualsOperator;
 import io.github.manuelarte.spring.queryparameter.operators.GreaterThanOperator;
 import io.github.manuelarte.spring.queryparameter.operators.GreaterThanOrEqualsOperator;
@@ -17,16 +19,26 @@ import io.github.manuelarte.spring.queryparameter.operators.LowerThanOrEqualsOpe
 import io.github.manuelarte.spring.queryparameter.operators.Operator;
 import io.github.manuelarte.spring.queryparameter.operators.queryprovider.OperatorQueryProvider;
 import io.github.manuelarte.spring.queryparameter.operators.queryprovider.OperatorQueryProviderImpl;
+import io.github.manuelarte.spring.queryparameter.transformers.QueryCriteriaTransformer;
 import io.github.manuelarte.spring.queryparameter.util.TriPredicate;
 import javax.persistence.criteria.Predicate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.domain.Specification;
 
 @Configuration
 @Import({QueryCriteriaConfig.class})
 public class JpaQueryParamConfig {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public QueryCriteriaTransformer<Specification<Object>> queryCriteriaToSpecificationTransformer(
+      final TypeTransformerProvider typeTransformerProvider,
+      final OperatorQueryProvider<OperatorPredicate<Object>, Predicate> operatorPredicateProvider) {
+    return new QueryCriteriaToSpecificationTransformer(typeTransformerProvider, operatorPredicateProvider);
+  }
 
   @Bean
   @ConditionalOnMissingBean
